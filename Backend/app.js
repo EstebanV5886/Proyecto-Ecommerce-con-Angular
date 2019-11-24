8b506778ffc25af27140185618291eb5ae8352e7
Esteban Varela <notificaciones@husky.com.pa>
2019-11-24 01:10:35 -0500

    Maquetacion Web con Bootstrap y comienzo Backend

---
+++ Backend/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+//Se cargan modulos de Node para crear servidor
+
+//Ejecutar Express (http)
+
+//Cargar ficheros rutas
---
+++ Backend/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/api_rest_ecommerce', { useNewUrlParser: true }).then(()=>{
+    console.log("Conexion exitosa");
+});
